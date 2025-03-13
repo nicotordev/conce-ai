691023069447a2e8a56b8fa057510aa44cd4a0e0
Nicolas <nicotordev@gmail.com>
2025-03-13 19:16:06 -0300

    Add AuthLayout component for authentication pages

---
+++ app/auth/layout.tsx
@@ -0,0 +1,18 @@
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="w-full">
+        <div className="p-4">
+          <h1 className="!font-bold !font-lato text-dark-text-accent text-xl">
+            CondorAI
+          </h1>
+        </div>
+      </div>
+      <main>{children}</main>;
+    </>
+  );
+}
