74694667908cd1f22976edb1649f79c9470154f7
Nicolas <nicotordev@gmail.com>
2025-03-12 21:08:47 -0300

    Update environment variable for Google Generative AI API key

--- lib/@google-generative-ai.ts
+++ lib/@google-generative-ai.ts
@@ -1,7 +1,7 @@
 import * as google from "@google/generative-ai";
 
 const genAI = new google.GoogleGenerativeAI(
-  process.env.NEXT_PUBLIC_GOOGLE_GENERATIVE_AI_API_KEY
+  process.env.GOOGLE_GENERATIVE_AI_API_KEY
 );
 
 
