a64ca5ea2b1295c6033560d2b7c202d0752a31bd
Nicolas <nicotordev@gmail.com>
2025-03-14 01:13:00 -0300

    Add routes constants for authentication and application navigation

---
+++ constants/routes.constants.ts
@@ -0,0 +1,24 @@
+const allRoutes = {
+  home: "/",
+  authSignIn: "/auth/sign-in",
+  authSignUp: "/auth/sign-up",
+  autnSignOut: "/auth/sign-out",
+  authForgotPassword: "/auth/forgot-password",
+  authResetPassword: "/auth/reset-password",
+  authVerifyEmail: "/auth/email-verification",
+};
+
+const routesConstants = {
+  allRoutes: allRoutes,
+  protectedPaths: {
+    authVerifyEmail: allRoutes.authVerifyEmail,
+  },
+  noAuth: {
+    authSignIn: allRoutes.authSignIn,
+    authSignUp: allRoutes.authSignUp,
+    authForgotPassword: allRoutes.authForgotPassword,
+  },
+  adminPaths: {},
+};
+
+export default routesConstants;
