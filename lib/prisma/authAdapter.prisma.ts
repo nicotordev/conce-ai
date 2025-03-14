38e3bc64d51cb42923b7b9f028afbffe589a9f5d
Nicolas <nicotordev@gmail.com>
2025-03-13 21:25:34 -0300

    Rename getUserByUsername to getUserByEmail and implement createUser method for user creation with role assignment

--- lib/prisma/authAdapter.prisma.ts
+++ lib/prisma/authAdapter.prisma.ts
@@ -1,15 +1,33 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./index.prisma";
+import { stripUndefined } from "@/utils/objects.utilts";
 
 const authAdapterPrisma = {
   ...PrismaAdapter(prisma),
-  getUserByUsername: async (username: string) => {
+  getUserByEmail: async (email: string): Promise<AdapterUser> => {
     const user = await prisma.user.findUnique({
       where: {
-        username,
+        email,
       },
     });
     return user;
+  },
+  createUser: async (
+    data: NonNullable<Partial<AdapterUser>>
+  ): Promise<AdapterUser> => {
+    const userData = { ...stripUndefined(data) };
+    return await prisma.user.create({
+      data: {
+        ...userData,
+        roleId: undefined,
+        Role: {
+          connectOrCreate: {
+            where: { name: "USER" },
+            create: { name: "USER" },
+          },
+        },
+      },
+    });
   },
 };
 
