ad49c13ec7dd94011449c99715a303d6f4bdd110
Nicolas <nicotordev@gmail.com>
2025-03-13 21:49:46 -0300

    Enhance SignUp component with loading states and error handling for user registration

--- components/Auth/SignUp.tsx
+++ components/Auth/SignUp.tsx
@@ -3,37 +3,67 @@
 import { SignUpProps } from "@/types/auth";
 import { SignUpPageStep } from "@/types/auth.enum";
 import { Transition } from "@headlessui/react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Input from "../Common/Forms/Input";
 import { BsEnvelope } from "react-icons/bs";
-import { doSignUpSteppedRedirection } from "@/app/actions/auth.actions";
+import {
+  doSignIn,
+  doSignUp,
+  doSignUpSteppedRedirection,
+} from "@/app/actions/auth.actions";
 import toast from "react-hot-toast";
 import PasswordInput from "../Common/Forms/PasswordInput";
+import authConstants from "@/constants/auth.constants";
 
 export default function SignUp({ state }: SignUpProps) {
   const step = state?.step ?? SignUpPageStep.email;
+  const [doingRedirection, setDoingRedirection] = useState(false);
+  const [loadingSignUp, setLoadingSignUp] = useState(false);
   const [email, setEmail] = useState(state?.email ?? "");
   const [password, setPassword] = useState(state?.password ?? "");
 
   async function handleNextStep(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const data = await doSignUpSteppedRedirection({
-      email,
-      password,
-      step: SignUpPageStep.password,
-    });
+    if (step === SignUpPageStep.email) {
+      setDoingRedirection(true);
+      const redirectionData = await doSignUpSteppedRedirection({
+        email,
+        password,
+        step: SignUpPageStep.password,
+      });
+      setDoingRedirection(false);
+      if (!redirectionData.success) {
+        toast.error("Ha ocurrido un error al intentar crear la cuenta");
+        return;
+      }
 
-    if (!data) {
-      toast.error("Ha ocurrido un error al intentar crear la cuenta");
-      return;
+      window.location.href = `/auth/sign-up?state=${redirectionData.data}`;
+    } else {
+      setLoadingSignUp(true);
+      const signUpResponse = await doSignUp({ email, password });
+      setLoadingSignUp(false);
+
+      if (signUpResponse.success) {
+        toast.success("Cuenta creada con éxito");
+        return await doSignIn({ email, password });
+      }
+
+      if (
+        signUpResponse.message ===
+        authConstants.ERROR_MESSAGES.USER_ALREADY_EXISTS
+      ) {
+        toast.error("El usuario ya existe");
+        return;
+      } else if (authConstants.ERROR_MESSAGES.INVALID_CREDENTIALS) {
+        toast.error("Credenciales inválidas");
+        return;
+      }
     }
-
-    window.location.href = `/auth/sign-up?state=${data}`;
   }
 
   return (
-    <form onSubmit={handleNextStep}>
+    <form onSubmit={handleNextStep} className="relative">
       <Transition
         show={step === SignUpPageStep.email}
         enter="transition ease-out duration-300"
@@ -56,6 +86,33 @@ export default function SignUp({ state }: SignUpProps) {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+        </div>
+      </Transition>
+      <Transition
+        show={loadingSignUp}
+        enter="transition ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        unmount={true}
+        as={Fragment}
+      >
+        <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center mt-4 z-50 pointer-events-none">
+          <div className="h-full flex flex-col w-full backdrop-blur-xs bg-white/10 scale-110">
+            <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
+              <div className="flex justify-center">
+                <div
+                  className="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
+                  role="status"
+                  aria-label="loading"
+                >
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Transition>
       <Transition
@@ -87,10 +144,12 @@ export default function SignUp({ state }: SignUpProps) {
             label="Contraseña"
             required
             setPassword={setPassword}
+            disabled={doingRedirection}
           />
         </div>
       </Transition>
       <button
+        disabled={doingRedirection || loadingSignUp}
         type="submit"
         className="cursor-pointer justify-center text-center mt-4 w-full py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary-600 text-white hover:bg-primary-600 focus:outline-hidden focus:bg-primary-700 disabled:opacity-50 disabled:pointer-events-none"
       >
