8c3b056b2e30e6ecb4e0ed47c2c44aaec4f5ed02
Nicolas <nicotordev@gmail.com>
2025-03-13 21:25:42 -0300

    Add SignUpState type to manage sign-up page state

--- types/auth.ts
+++ types/auth.ts
@@ -1,8 +1,15 @@
+import { SignUpPageStep } from "./auth.enum";
+type SignUpState = {
+  step: SignUpPageStep;
+  email: string;
+  password: string;
+} | null;
 type SignUpProps = {
   state: {
+    step: SignUpPageStep;
     email: string;
     password: string;
   } | null;
 };
 
-export type { SignUpProps };
+export type { SignUpState, SignUpProps };
