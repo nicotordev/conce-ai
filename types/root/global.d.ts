c86c174f1e0f81f4434efc565b82ea1b8ba42455
Nicolas <nicotordev@gmail.com>
2025-03-14 18:29:55 -0300

    Add environment variables for API and email configuration

--- types/root/global.d.ts
+++ types/root/global.d.ts
@@ -1,4 +1,3 @@
-
 declare global {
   interface Window extends Window {
     grecaptcha: {
@@ -12,12 +11,20 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
+      NEXT_PUBLIC_BASE_URL: string;
+      NEXT_PUBLIC_API_URL: string;
       NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
       RECAPTCHA_SECRET_KEY: string;
       GOOGLE_GENERATIVE_AI_API_KEY: string;
       GOOGLE_CLIENT_ID: string;
       GOOGLE_CLIENT_SECRET: string;
       ENCRYPTION_KEY: string;
+      BREVO_SENDER_NAME: string;
+      BREVO_SENDER_EMAIL: string;
+      BREVO_SMTP_SERVER: string;
+      BREVO_SMTP_PORT: string;
+      BREVO_SMTP_LOGIN: string;
+      BREVO_SMTP_PASSWORD: string;
     }
   }
 
