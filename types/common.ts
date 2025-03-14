922397f84cefa51be81a3b07bee3d0dca3efdcf7
Nicolas <nicotordev@gmail.com>
2025-03-13 21:25:46 -0300

    Add InputProps and PasswordInputProps types for enhanced input handling

---
+++ types/common.ts
@@ -0,0 +1,29 @@
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  rightIcon?: React.ReactNode;
+  leftIcon?: React.ReactNode;
+  label?: string;
+  error?: string;
+  validations?: {
+    title?: string;
+    items: Array<{
+      isValid: boolean;
+      message: string;
+    }>;
+    show?: boolean;
+  };
+};
+type PasswordInputProps = {
+  label?: string;
+  setPassword: (password: string) => void;
+} & Omit<
+  React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >,
+  "value" | "defaultValue"
+>;
+
+export type { InputProps, PasswordInputProps };
