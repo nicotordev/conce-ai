402d6c922d773d570d50ab10c970b49722c4b2a4
Nicolas <nicotordev@gmail.com>
2025-03-14 18:30:07 -0300

    Add EmailVerificationPage component for user email verification

---
+++ app/auth/email-verification/page.tsx
@@ -0,0 +1,34 @@
+import EmailVerification from "@/components/Auth/EmailVerification";
+import { decryptData } from "@/lib/crypto";
+import { EmailVerificationState } from "@/types/auth";
+import { PagePropsCommon } from "@/types/pages";
+import Link from "next/link";
+
+export default async function EmailVerificationPage(props: PagePropsCommon) {
+  const _searchParams = await props.searchParams;
+  const state: EmailVerificationState | null =
+    typeof _searchParams.state === "string"
+      ? decryptData<EmailVerificationState>(_searchParams.state)
+      : null;
+
+  return (
+    <div className="min-h-screen w-full">
+      <div className="mx-auto max-w-xs mt-16">
+        <h2 className="!font-paragraph font-bold text-dark-text-primary text-3xl text-center">
+          Bienvenido
+        </h2>
+        <EmailVerification state={state} />
+
+        <div className="mt-12 flex items-center justify-center gap-2 text-xs fixed bottom-12 left-1/2 -translate-x-1/2">
+          <Link href="/auth/sign-up" className="text-primary-600">
+            Terminos de uso
+          </Link>
+          |
+          <Link href="/auth/sign-up" className="text-primary-600">
+            Politica y privacidad
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
