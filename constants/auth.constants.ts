7db1c0017441cd96dd0ecf2137e9856152614e89
Nicolas <nicotordev@gmail.com>
2025-03-14 19:39:31 -0300

    Refactor auth constants to separate error and success message codes

--- constants/auth.constants.ts
+++ constants/auth.constants.ts
@@ -1,7 +1,7 @@
 const authConstants = {
   SESSION_MAX_AGE: 60 * 60 * 24 * 30,
   PROTECTED_PATHS: ["/dashboard"],
-  ERROR_MESSAGES: {
+  ERROR_MESSAGES_CODES: {
     INVALID_CREDENTIALS: "invalid-credentials",
     USER_NOT_FOUND: "user-not-found",
     USER_ALREADY_EXISTS: "user-exists",
@@ -9,12 +9,14 @@ const authConstants = {
     INTERNAL_SERVER_ERROR: "internal-server-error",
     INVALID_VERIFICATION_TOKEN: "invalid-verification-token",
     EXPIRED_VERIFICATION_TOKEN: "expired-verification-token",
+    INVALID_USER: "invalid user",
   },
-  SUCCESS_MESSAGES: {
+  SUCCESS_MESSAGES_CODES: {
     SIGN_UP: "account-created",
     SIGN_IN: "signed-in",
     SIGN_OUT: "signed-out",
     SIGN_UP_REDIRECT: "account-created-redirect",
+    EMAIL_RESEND: "email-resend",
   }
 };
 export default authConstants;
