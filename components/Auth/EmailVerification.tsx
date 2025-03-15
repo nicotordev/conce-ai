443d702ca3baf428f9d66936f2afd2580f4c67cb
Nicolas <nicotordev@gmail.com>
2025-03-15 13:25:34 -0300

    Refactor EmailVerification component to integrate AuthLoading, AuthError, and AuthTokenExpired for improved user feedback and loading states

--- components/Auth/EmailVerification.tsx
+++ components/Auth/EmailVerification.tsx
@@ -2,18 +2,19 @@
 import { EmailVerificationProps } from "@/types/auth";
 import { EmailVerificationStep } from "@/types/auth.enum";
 import { Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import {  useState } from "react";
 import CondorInput from "../Common/Forms/CondorInput";
 import { BsLock } from "react-icons/bs";
-import { CgCalendar, CgCheck, CgDanger } from "react-icons/cg";
 import {
   doEmailVerification,
   doSteppedRedirection,
 } from "@/app/actions/auth.actions";
 import Link from "next/link";
-import authConstants from "@/constants/auth.constants";
 import { Button } from "../ui/button";
 import { EmailVerificationModal } from "./EmailVerificationModal";
+import AuthLoading from "./AuthLoading";
+import AuthError from "./AuthError";
+import AuthTokenExpired from "./AuthTokenExpired";
 export default function EmailVerification({ state }: EmailVerificationProps) {
   const step = state?.step || EmailVerificationStep.start;
   const error = state?.error || "";
@@ -64,7 +65,6 @@ export default function EmailVerification({ state }: EmailVerificationProps) {
               id="code"
               placeholder="Código de verificación"
               type="text"
-              label="Código de verificación"
               required
               leftIcon={<BsLock />}
               value={code}
@@ -73,112 +73,11 @@ export default function EmailVerification({ state }: EmailVerificationProps) {
             />
           </div>
         </Transition>
-        <Transition
-          show={loading}
-          enter="transition ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          unmount={true}
-          as={Fragment}
-        >
-          <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center mt-4 z-50 pointer-events-none">
-            <div className="h-full flex flex-col w-full backdrop-blur-xs bg-white/10 scale-110">
-              <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
-                <div className="flex justify-center">
-                  <div
-                    className="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
-                    role="status"
-                    aria-label="loading"
-                  >
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Transition>
-        <Transition
-          show={step === EmailVerificationStep.success && !loading}
-          enter="transition ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          unmount={true}
-        >
-          <div>
-            {/* Error UI */}
-            <div className="flex flex-col gap-2 text-center bg-white rounded-md border border-solid border-gray-300 p-4 my-4">
-              <p className="!text-silver-700 font-semibold text-xs flex flex-col gap-1">
-                <strong className="!text-green-500 !text-sm !font-medium flex items-center justify-center gap-2">
-                  <CgCheck />{" "}
-                  {error ===
-                  authConstants.ERROR_MESSAGES_CODES.INVALID_VERIFICATION_TOKEN
-                    ? "Código de verificación incorrecto"
-                    : "Código de verificación correcto"}
-                </strong>
-                {error ===
-                authConstants.ERROR_MESSAGES_CODES.INVALID_VERIFICATION_TOKEN
-                  ? "El código de verificación ingresado no es correcto. Por favor, verifica el código e intenta nuevamente."
-                  : "Ha ocurrido un error interno. Por favor, intenta nuevamente."}
-                <small>
-                  <strong>Nota:</strong> Serás redirigido a la aplicación en
-                  unos segundos
-                </small>
-              </p>
-            </div>
-          </div>
-        </Transition>
-        <Transition
-          show={Boolean(step === EmailVerificationStep.expired && !loading)}
-          enter="transition ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          unmount={true}
-        >
-          <div>
-            {/* Error UI */}
-            <div className="flex flex-col gap-2 text-center bg-white rounded-md border border-solid border-gray-300 p-4 my-4">
-              <p className="!text-silver-700 font-semibold text-xs flex flex-col gap-1">
-                <strong className="!text-red-500 !text-sm !font-medium flex items-center justify-center gap-2">
-                  <CgCalendar /> Código de verificación Expirado
-                </strong>
-                El código de verificación ingresado ha expirado. Hemos enviado
-                un nuevo código de verificación a tu correo electrónico.
-              </p>
-            </div>
-          </div>
-        </Transition>
-        <Transition
-          show={Boolean(step === EmailVerificationStep.error && !loading)}
-          enter="transition ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          unmount={true}
-        >
-          <div>
-            {/* Error UI */}
-            <div className="flex flex-col gap-2 text-center bg-white rounded-md border border-solid border-gray-300 p-4 my-4">
-              <p className="!text-silver-700 font-semibold text-xs flex flex-col gap-1">
-                <strong className="!text-red-500 !text-sm !font-medium flex items-center justify-center gap-2">
-                  <CgDanger /> Código de verificación incorrecto
-                </strong>
-                El código de verificación ingresado no es correcto. Por favor,
-                verifica el código e intenta nuevamente.
-              </p>
-            </div>
-          </div>
-        </Transition>
+        <AuthLoading loading={loading} />
+        <AuthError error={error} />
+        <AuthTokenExpired
+          show={Boolean(step === EmailVerificationStep.expired)}
+        />
         <div className="mt-4">
           {Boolean(step === EmailVerificationStep.expired) ? null : Boolean(
               step === EmailVerificationStep.error
