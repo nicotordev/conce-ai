7d863b291cce4e3f9824564a4a77d1cdbcd5987e
Nicolas <nicotordev@gmail.com>
2025-03-15 16:40:38 -0300

    Refactor GoogleGenerativeAI to integrate FetchClient for API interactions and enhance model retrieval

--- lib/@google-generative-ai.ts
+++ lib/@google-generative-ai.ts
@@ -1,8 +1,31 @@
 import * as google from "@google/generative-ai";
+import FetchClient from "./fetch-client";
+import { GoogleModel } from "@/types/@google-generative-ai";
 
-const genAI = new google.GoogleGenerativeAI(
-  process.env.GOOGLE_GENERATIVE_AI_API_KEY
-);
+class GoogleGenerativeAI {
+  private googleGenerativeAiApiKey: string =
+    process.env.GOOGLE_GENERATIVE_AI_API_KEY;
+  private googleGenerativeAiApiUrl: string =
+    process.env.GOOGLE_GENERATIVE_AI_API_URL;
+  public genAI: google.GoogleGenerativeAI = new google.GoogleGenerativeAI(
+    this.googleGenerativeAiApiKey
+  );
+  private fetchClient: FetchClient = new FetchClient(
+    this.googleGenerativeAiApiUrl
+  );
+  public get = this.fetchClient.get.bind(this.fetchClient);
+  public post = this.fetchClient.post.bind(this.fetchClient);
 
+  public async getModels(): Promise<GoogleModel[]> {
+    try {
+      const { models } = await this.get<{ models: GoogleModel[] }>("/models");
+      return models;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
 
-export default genAI;
+const googleGenerativeAI = new GoogleGenerativeAI();
+
+export default googleGenerativeAI;
