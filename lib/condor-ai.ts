512e42ed5ebd972d2cc3b7dfd500a7fba6be8b84
Nicolas <nicotordev@gmail.com>
2025-03-15 10:52:30 -0300

    Add CRUD methods to CondorAI class for enhanced API interaction

--- lib/condor-ai.ts
+++ lib/condor-ai.ts
@@ -11,13 +11,155 @@ class CondorAI {
     endpoint: string | URL,
     config: RequestInit = {},
     request?: NextRequest
-  ) {
+  ): Promise<T> {
     try {
       const requestInit: RequestInit = {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
+        ...config,
+      };
+
+      if (request) {
+        requestInit.headers = {
+          Cookie: request.headers.get("cookie") || "",
+          Authorization: request.headers.get("authorization") || "",
+        };
+      }
+
+      const response = await fetch(`${this.apiUrl}${endpoint}`, requestInit);
+
+      if (!response.ok) {
+        throw new CondorAIError(response.statusText);
+      }
+      return response.json() as Promise<T>;
+    } catch (err) {
+      throw new CondorAIError(
+        err instanceof Error ? err.message : "Unknown error"
+      );
+    }
+  }
+
+  private async post<T, P>(
+    endpoint: string | URL,
+    data: P,
+    config: RequestInit = {},
+    request?: NextRequest
+  ) {
+    try {
+      const requestInit: RequestInit = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        ...config,
+      };
+
+      if (request) {
+        requestInit.headers = {
+          Cookie: request.headers.get("cookie") || "",
+          Authorization: request.headers.get("authorization") || "",
+        };
+      }
+
+      const response = await fetch(`${this.apiUrl}${endpoint}`, requestInit);
+
+      if (!response.ok) {
+        throw new CondorAIError(response.statusText);
+      }
+      return response.json() as Promise<T>;
+    } catch (err) {
+      throw new CondorAIError(
+        err instanceof Error ? err.message : "Unknown error"
+      );
+    }
+  }
+
+  private async put<T, P>(
+    endpoint: string | URL,
+    data: P,
+    config: RequestInit = {},
+    request?: NextRequest
+  ) {
+    try {
+      const requestInit: RequestInit = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        ...config,
+      };
+
+      if (request) {
+        requestInit.headers = {
+          Cookie: request.headers.get("cookie") || "",
+          Authorization: request.headers.get("authorization") || "",
+        };
+      }
+
+      const response = await fetch(`${this.apiUrl}${endpoint}`, requestInit);
+
+      if (!response.ok) {
+        throw new CondorAIError(response.statusText);
+      }
+      return response.json() as Promise<T>;
+    } catch (err) {
+      throw new CondorAIError(
+        err instanceof Error ? err.message : "Unknown error"
+      );
+    }
+  }
+
+  private async delete<T>(
+    endpoint: string | URL,
+    config: RequestInit = {},
+    request?: NextRequest
+  ) {
+    try {
+      const requestInit: RequestInit = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        ...config,
+      };
+
+      if (request) {
+        requestInit.headers = {
+          Cookie: request.headers.get("cookie") || "",
+          Authorization: request.headers.get("authorization") || "",
+        };
+      }
+
+      const response = await fetch(`${this.apiUrl}${endpoint}`, requestInit);
+
+      if (!response.ok) {
+        throw new CondorAIError(response.statusText);
+      }
+      return response.json() as Promise<T>;
+    } catch (err) {
+      throw new CondorAIError(
+        err instanceof Error ? err.message : "Unknown error"
+      );
+    }
+  }
+
+  private async patch<T, D>(
+    endpoint: string | URL,
+    data: D,
+    config: RequestInit = {},
+    request?: NextRequest
+  ) {
+    try {
+      const requestInit: RequestInit = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
         ...config,
       };
 
@@ -49,10 +191,41 @@ class CondorAI {
       throw err;
     }
   }
+
+  public async encryptData<T extends object>(data: T): Promise<string> {
+    try {
+      const { data: encryptedData } = await this.post<
+        BaseApiResponse<string>,
+        unknown
+      >("/crypto/encrypt", data, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-condor-ai-key": process.env.CONDOR_AI_API_KEY || "",
+        },
+      });
+      return encryptedData;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async decryptData<T>(encryptedData: string): Promise<T> {
+    try {
+      const { data: decryptedData } = await this.get<BaseApiResponse<T>>(
+        `/crypto/decrypt?encryption=${encryptedData}`,
+        {
+          headers: {
+            "x-condor-ai-key": process.env.CONDOR_AI_API_KEY || "",
+          },
+        }
+      );
+      return decryptedData;
+    } catch (err) {
+      throw err;
+    }
+  }
 }
 
-
 const condorAi = new CondorAI();
-
 
 export default condorAi;
