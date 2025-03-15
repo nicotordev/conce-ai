c333c0b36221915b2504bf88f065fb747d183858
Nicolas <nicotordev@gmail.com>
2025-03-15 13:25:15 -0300

    Add Reset Password page with state handling and navigation

---
+++ app/auth/reset-password/page.tsx
@@ -0,0 +1,58 @@
+import ResetPassword from "@/components/Auth/ResetPassword";
+import { decryptData } from "@/lib/crypto";
+import { ResetPasswordState } from "@/types/auth";
+import { ResetPasswordStep } from "@/types/auth.enum";
+import { PagePropsCommon } from "@/types/pages";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function ResetPasswordPage({
+  searchParams,
+}: PagePropsCommon) {
+  const _searchParams = await searchParams;
+  const state: ResetPasswordState | null =
+    typeof _searchParams.state === "string"
+      ? decryptData<ResetPasswordState>(_searchParams.state)
+      : null;
+
+  if ((await _searchParams.email) || (await _searchParams.password)) {
+    /* Send other params except by email and password */
+    const searchParamsQuery = new URLSearchParams();
+    await Promise.all(
+      Object.keys(_searchParams).map(async (key) => {
+        if (key !== "email" && key !== "password") {
+          searchParamsQuery.append(key, (await _searchParams[key]) as string);
+        }
+      })
+    );
+
+    redirect(`/auth/reset-password?${searchParamsQuery.toString()}`);
+  }
+
+  return (
+    <div className="min-h-screen w-full">
+      <div className="mx-auto max-w-xs mt-16">
+        <ResetPassword
+          state={{
+            ...state,
+            step: state?.step ? state?.step : ResetPasswordStep.email,
+            email: state?.email ?? "",
+            error: state?.error ?? "",
+            password: state?.password ?? "",
+            token: state?.token ?? "",
+          }}
+        />
+
+        <div className="mt-12 flex items-center justify-center gap-2 text-xs">
+          <Link href="/auth/sign-up" className="text-primary-600">
+            Terminos de uso
+          </Link>
+          |
+          <Link href="/auth/sign-up" className="text-primary-600">
+            Politica y privacidad
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
