10f939c5fad8ff5b720143b33921051acd166b66
Nicolas <nicotordev@gmail.com>
2025-03-15 18:47:58 -0300

    Add logging for error handling in GoogleGenerativeAI getModels method

--- lib/@google-generative-ai.ts
+++ lib/@google-generative-ai.ts
@@ -1,6 +1,7 @@
 import * as google from "@google/generative-ai";
 import FetchClient from "./fetch-client";
 import { GoogleModel } from "@/types/@google-generative-ai";
+import logger from "./logger";
 
 class GoogleGenerativeAI {
   private googleGenerativeAiApiKey: string =
@@ -18,9 +19,12 @@ class GoogleGenerativeAI {
 
   public async getModels(): Promise<GoogleModel[]> {
     try {
-      const { models } = await this.get<{ models: GoogleModel[] }>("/models");
+      const { models } = await this.get<{ models: GoogleModel[] }>(
+        `/models?key=${process.env.GOOGLE_GENERATIVE_AI_API_KEY}`
+      );
       return models;
     } catch (err) {
+      logger.error(`[ERROR-GOOGLE-GENERATIVE-AI-GET-MODELS]`, err);
       throw err;
     }
   }
