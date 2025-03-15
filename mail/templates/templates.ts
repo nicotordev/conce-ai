31cc094a1ddda93d70af84ac8bc364a8385f792a
Nicolas <nicotordev@gmail.com>
2025-03-15 13:26:11 -0300

    Add email templates module to consolidate email templates for password reset, verification, and welcome

---
+++ mail/templates/templates.ts
@@ -0,0 +1,11 @@
+import emailPasswordReset from "./emailPasswordReset";
+import emailVerification from "./emailVerification";
+import emailWelcome from "./emailWelcome";
+
+const templates = {
+  emailPasswordReset,
+  emailVerification,
+  emailWelcome,
+};
+
+export default templates;
