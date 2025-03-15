da504ab98848dde7ee98c78bffeba367c93b5a57
Nicolas <nicotordev@gmail.com>
2025-03-15 13:25:43 -0300

    Refactor SignIn component to integrate AuthLoading and AuthError for improved loading and error handling; replace password reset button with a link to the reset password page.

--- components/Auth/SignIn.tsx
+++ components/Auth/SignIn.tsx
@@ -8,8 +8,9 @@ import CondorInput from "../Common/Forms/CondorInput";
 import { BsEnvelope, BsLock } from "react-icons/bs";
 import { doSignIn, doSteppedRedirection } from "@/app/actions/auth.actions";
 import toast from "react-hot-toast";
-import { CgDanger } from "react-icons/cg";
-import { getSignInErrorMessage } from "@/utils/language.utilts";
+import Link from "next/link";
+import AuthError from "./AuthError";
+import AuthLoading from "./AuthLoading";
 
 export default function SignIn({ state }: SignInProps) {
   const step = state?.step ?? SignInPageStep.email;
@@ -57,29 +58,10 @@ export default function SignIn({ state }: SignInProps) {
           ? "Bienvenido"
           : "Introducte tu contraseña"}
       </h2>
-      <form onSubmit={handleNextStep} className="relative">
-        <Transition
-          show={Boolean(error)}
-          enter="transition ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          unmount={true}
-        >
-          <div>
-            {/* Error UI */}
-            <div className="flex flex-col gap-2 text-center bg-white rounded-md border border-solid border-gray-300 p-4 my-4">
-              <p className="!text-silver-700 font-semibold text-xs flex flex-col gap-1">
-                <strong className="!text-red-500 !text-sm !font-medium flex items-center justify-center gap-2">
-                  <CgDanger /> Ocurrio un error
-                </strong>
-                {getSignInErrorMessage(error)}
-              </p>
-            </div>
-          </div>
-        </Transition>
+      <form onSubmit={handleNextStep} className="relative mt-4">
+        <AuthLoading loading={loadingSignUp} />
+        <AuthError error={error} />
+
         <Transition
           show={step === SignInPageStep.email}
           enter="transition ease-out duration-300"
@@ -96,40 +78,12 @@ export default function SignIn({ state }: SignInProps) {
               id="email"
               placeholder="Dirección de correo electrónico"
               type="email"
-              label="Correo electrónico"
               required
               leftIcon={<BsEnvelope />}
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               disabled={doingRedirection || loadingSignUp}
             />
-          </div>
-        </Transition>
-        <Transition
-          show={loadingSignUp}
-          enter="transition ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          unmount={true}
-          as={Fragment}
-        >
-          <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center mt-4 z-50 pointer-events-none">
-            <div className="h-full flex flex-col w-full backdrop-blur-xs bg-white/10 scale-110">
-              <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
-                <div className="flex justify-center">
-                  <div
-                    className="animate-spin inline-block size-6 border-3 border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
-                    role="status"
-                    aria-label="loading"
-                  >
-                    <span className="sr-only">Cargando...</span>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </Transition>
         <Transition
@@ -155,12 +109,13 @@ export default function SignIn({ state }: SignInProps) {
             />
             <div className="flex flex-col-reverse">
               <div className="flex items-center justify-start">
-                <button
+                <Link
+                  href="/auth/reset-password"
                   type="button"
                   className="w-fit text-xs cursor-pointer justify-center text-center py-1.5 pr-4 inline-flex items-center gap-x-2 font-medium rounded-lg border border-transparent text-primary-600 hover:text-primary-900 disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:text-primary-600 transition-all duration-300"
                 >
                   ¿Olvidaste tu contraseña?
-                </button>
+                </Link>
               </div>
               <CondorInput
                 name="password"
