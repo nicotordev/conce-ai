9fd8e767fa9af8ea517b81312df32f50e39e25be
Nicolas <nicotordev@gmail.com>
2025-03-16 18:39:43 -0300

    Add CondorAIProvider component with context and model selection logic

---
+++ providers/CondorAIProvider.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { CondorAIContextType, CondorAIProviderProps } from "@/types/providers";
+import { useModelsQuery } from "@/useQuery/mutations/users.mutations";
+import { AppNavModel } from "@/types/layout";
+// Crea el contexto
+const CondorAIContext = createContext<CondorAIContextType | null>(null);
+
+// Hook personalizado para usar el contexto en los componentes hijos
+export const useCondorAI = () => {
+  const context = useContext(CondorAIContext);
+  if (!context) {
+    throw new Error(
+      "useRecaptcha debe ser usado dentro de un CondorAIProvider"
+    );
+  }
+  return context;
+};
+
+export const CondorAIProvider = ({ children }: CondorAIProviderProps) => {
+  const modelsQuery = useModelsQuery();
+  const [selectedModel, setSelectedModel] = useState<AppNavModel | null>(null);
+
+  useEffect(() => {
+    if (modelsQuery.data && modelsQuery.data.length > 0 && !selectedModel) {
+      setSelectedModel(modelsQuery.data[0]);
+    }
+  }, [modelsQuery.data, selectedModel]);
+
+  return (
+    <CondorAIContext.Provider
+      value={{
+        models: modelsQuery.data || [],
+        selectedModel: selectedModel,
+        isLoading: modelsQuery.isLoading,
+        setSelectedModel,
+      }}
+    >
+      {children}
+    </CondorAIContext.Provider>
+  );
+};
