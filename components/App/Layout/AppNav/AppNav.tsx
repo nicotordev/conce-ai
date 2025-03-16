9817c97a2bc5d314fea7c5492c4a6e1cf093d906
Nicolas <nicotordev@gmail.com>
2025-03-16 20:26:44 -0300

    Add button to open conversations in AppNav for improved user interaction

--- components/App/Layout/AppNav/AppNav.tsx
+++ components/App/Layout/AppNav/AppNav.tsx
@@ -6,17 +6,31 @@ import { TbMessageCircle } from "react-icons/tb";
 import { BsFillShareFill } from "react-icons/bs";
 import AppNavModelsDropdownProps from "./AppNavModelsDropdown";
 import AppNavUserDropdown from "./AppNavUserDropdown";
+import { useCondorAI } from "@/providers/CondorAIProvider";
 export default function AppNav({ session }: AppNavProps) {
+  const { conversations } = useCondorAI();
   return (
     <header>
       <nav className="px-4 py-3">
         <div className="flex items-center justify-between">
           <ul className="flex items-center gap-6 text-2xl text-dark-text-accent">
-            <li>
-              <MdOutlineSpaceDashboard />
+            <li className="flex items-center justify-center">
+              <Button
+                variant={"outline"}
+                onClick={() => conversations.setConversationsOpen(true)}
+                className="border-none shadow-none relative group"
+              >
+                <div
+                  aria-hidden="true"
+                  className="w-2 h-2 bg-primary-600 absolute top-1.5 right-3 rounded-full group-hover:bg-primary-400 transition-colors duration-300"
+                ></div>
+                <MdOutlineSpaceDashboard className="!w-6 !h-6" />
+              </Button>
             </li>
-            <li>
-              <TbMessageCircle />
+            <li className="flex items-center justify-center">
+              <Button variant={"outline"} className="border-none shadow-none">
+                <TbMessageCircle className="!w-6 !h-6" />
+              </Button>
             </li>
             <li>
               <AppNavModelsDropdownProps />
