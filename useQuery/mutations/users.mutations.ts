0ff76e91694483e22d3d284943a8c8426b058eed
Nicolas <nicotordev@gmail.com>
2025-03-16 18:39:14 -0300

    Add useModelsQuery hook to fetch user models using React Query

---
+++ useQuery/mutations/users.mutations.ts
@@ -0,0 +1,13 @@
+import condorAi from "@/lib/condor-ai";
+import { useQuery } from "@tanstack/react-query";
+
+function useModelsQuery() {
+  const modelsQuery = useQuery({
+    queryKey: ["users/models"],
+    queryFn: condorAi.getModels,
+  });
+
+  return modelsQuery;
+}
+
+export { useModelsQuery };
