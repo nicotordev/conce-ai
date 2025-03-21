ce4fc382f778cb67a6b0e19b883fdf5643308061
Nicolas <nicotordev@gmail.com>
2025-03-21 18:29:23 -0300

    Add conversations constants for error message codes

---
+++ constants/conversations.constants.ts
@@ -0,0 +1,11 @@
+const conversationsConstants = {
+    ERROR_MESSAGES_CODES: {
+        MESSAGE_IS_NOT_VALID: "message-is-not-valid",
+        MODEL_IS_NOT_VALID: "model-is-not-valid",
+        INTERNAL_SERVER_ERROR: "internal-server-error",
+        USER_NOT_AUTHORIZED: "user-not-authorized",
+    }
+}
+
+
+export default conversationsConstants;
