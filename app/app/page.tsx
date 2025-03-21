9bed4a43aafc09efe79e97a341fc687e104bba37
Nicolas <nicotordev@gmail.com>
2025-03-21 18:29:00 -0300

    Add AppNewConversation component and state management to AppPage

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,23 @@
+import AppNewConversation from "@/components/App/AppNewConversation";
+import { decryptData } from "@/lib/crypto";
+import { AppNewConversationState } from "@/types/app";
+import { PagePropsCommon } from "@/types/pages";
 
-export default async function AppPage() {
-  return <></>;
+export default async function AppPage({ searchParams }: PagePropsCommon) {
+  const _searchParams = await searchParams;
+  const state: AppNewConversationState | null =
+    typeof _searchParams.state === "string"
+      ? decryptData<AppNewConversationState>(_searchParams.state)
+      : null;
+
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <AppNewConversation
+        state={{
+          ...state,
+          error: state?.error || "",
+        }}
+      />
+    </div>
+  );
 }
