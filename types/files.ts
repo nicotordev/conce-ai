40ea2e35b2b7c4ef0f8098bb19b89833dd14cf68
Nicolas <nicotordev@gmail.com>
2025-03-22 09:04:29 -0300

    Add CondorAIFile model and update file type definitions for enhanced file management

--- types/files.ts
+++ types/files.ts
@@ -2,6 +2,12 @@ import { NicoDropzoneFile } from "@nicotordev/nicodropzone/dist/types";
 import { IconType } from "react-icons/lib";
 
 type CondorAIFile = NicoDropzoneFile & {
+  name: string;
+  src: string;
+  type: string;
+  nameWithoutExtension: string;
+  sizeInMB: number;
+  preview: string;
   icon: IconType;
   status: "uploading" | "uploaded" | "error";
 };
