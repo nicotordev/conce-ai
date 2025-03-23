8cba4b314db87060108acdcc5737e27eacf80fe6
Nicolas <nicotordev@gmail.com>
2025-03-23 10:00:36 -0300

    Integrate Google Generative AI for dynamic news image prompt generation

--- lib/cloudfare.ts
+++ lib/cloudfare.ts
@@ -4,17 +4,31 @@ import logger from "./consola/logger";
 import { v4 } from "uuid";
 import nicodropzone from "./@nicotordev/nicodropzone";
 import { fileTypeFromBuffer } from "file-type";
+import googleGenerativeAI from "./@google-generative-ai";
 
 async function generateNewsImage(news: News) {
-  const prompt = `Genera una imagen de la noticia "${JSON.stringify(
-    news,
-    null,
-    2
-  )}`;
+  const model = googleGenerativeAI.genAI.getGenerativeModel({
+    model: "models/gemini-2.0-flash-001",
+  });
+
+  const chatSession = model.startChat({});
+
+  const response = await chatSession.sendMessage(
+    `Eres un generador de prompts para modelos de texto a imagen. A partir del contenido de una noticia, tu tarea es crear un prompt detallado, en lenguaje natural, para generar una imagen que represente visualmente dicha noticia.
+  
+  Incluye los elementos clave como contexto, personajes, locación, ambiente, emociones o tono, si son relevantes. Evita repetir literalmente el texto de la noticia y enfócate en describir la imagen que se debería generar.
+  
+  Noticia:
+  ${JSON.stringify(news, null, 2)}
+  
+  Devuélveme solo el prompt.`
+  );
+
+  const prompt = response.response.text().trim();
 
   try {
     const response = await axios.post(
-      "https://api.cloudflare.com/client/v4/accounts/48a9712a4636052342824b8ef20aab89/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0",
+      "https://api.cloudflare.com/client/v4/accounts/48a9712a4636052342824b8ef20aab89/ai/run/@cf/lykon/dreamshaper-8-lcm",
       {
         prompt: prompt,
         negative_prompt:
@@ -30,7 +44,6 @@ async function generateNewsImage(news: News) {
         responseType: "arraybuffer", // 👈 clave para recibir binario
       }
     );
-
 
     const buffer = Buffer.from(response.data);
 
