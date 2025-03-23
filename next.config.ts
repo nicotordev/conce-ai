6b52f4861a0ce2f0de794c769b410db11c3ee5f6
Nicolas <nicotordev@gmail.com>
2025-03-23 09:48:15 -0300

    Add remote image pattern for assets.nicotordev.com in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.nicotordev.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },};
 
 export default nextConfig;
