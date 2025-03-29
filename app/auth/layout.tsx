e721759e6e47803f6b4b23acced60db827e3bd93
Nicolas <nicotordev@gmail.com>
2025-03-29 07:22:52 -0300

    Refactor AuthLayout component: simplify layout structure by removing unnecessary elements and returning only children for a cleaner implementation.

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,23 +1,7 @@
-import AuthFooter from "@/components/Auth/AuthFooter";
-
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="min-h-screen w-full bg-white dark:bg-shark-950 text-black dark:text-white transition-colors">
-      <header className="w-full">
-        <div className="p-4">
-          <h1 className="!font-bold !font-lato text-[--color-dark-text-accent] dark:text-white text-xl">
-            Conce.ai
-          </h1>
-        </div>
-      </header>
-
-      <main>{children}</main>
-
-      <AuthFooter />
-    </div>
-  );
+  return children
 }
